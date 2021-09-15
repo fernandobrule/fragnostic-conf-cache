a25991c60f72c312c2776bca475833c7392ade40
Fernando Brûlé <fernandobrule@gmail.com>
2021-09-14 21:44:08 -0300

    fix deps version

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,12 +3,12 @@ import Keys._
 
 object Dependencies {
 
-  lazy val fragnosticConfEnv = "com.fragnostic"   %  "fragnostic-conf-env_2.13"   % "0.1.5"
+  lazy val fragnosticConfEnv = "com.fragnostic"   %  "fragnostic-conf-env_2.13"   % "0.1.7"
 
   lazy val lettuce =           "io.lettuce"       %  "lettuce-core"               % "6.1.2.RELEASE"
 
-  lazy val logbackClassic      = "ch.qos.logback" % "logback-classic"            % "1.2.3" % "runtime"
-  lazy val slf4jApi            = "org.slf4j"      % "slf4j-api"                  % "1.7.25" % "runtime"
-  lazy val scalatest           = "org.scalatest" %% "scalatest"                  % "3.2.2" % "test"
+  lazy val logbackClassic      = "ch.qos.logback" % "logback-classic"             % "1.2.3" % "runtime"
+  lazy val slf4jApi            = "org.slf4j"      % "slf4j-api"                   % "1.7.25" % "runtime"
+  lazy val scalatest           = "org.scalatest" %% "scalatest"                   % "3.2.2" % "test"
 
 }
